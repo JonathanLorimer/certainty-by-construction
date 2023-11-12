90a54b7a7abdeafee66ae479e22f40e66be19c8f
Jonathan Lorimer <jonathan_lorimer@mac.com>
2023-11-12 15:06:47 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "cbc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    cornelis.url = "github:isovector/cornelis";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    cornelis,
+  }:
+    with flake-utils.lib;
+    eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      {
+        # nix develop
+        devShell =
+          pkgs.mkShell {
+            buildInputs = [
+              cornelis.packages.${system}.cornelis
+              cornelis.packages.${system}.agda
+            ];
+          };
+      });
+}
